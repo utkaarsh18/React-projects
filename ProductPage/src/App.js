8f4622e9db627c7b07fcfb800fdf3682e9675a8a
Utkaarsh Bhaskarwar <65220489+utkaarsh18@users.noreply.github.com>
2024-03-27 18:11:51 -0500

    Add files via upload

---
+++ ProductPage/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo"/>
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">Contact</li>
+          <li href="#">About</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ ProductPage/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Hero from './Hero';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+     <App></App>
+     <Hero></Hero>
+  </>
+
+
+);
+
